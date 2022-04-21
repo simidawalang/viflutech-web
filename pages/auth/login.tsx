9cb6e0e6f23bf87929df3f5903be50cca02e17aa
simidawalang <simidawalang@gmail.com>
2022-04-21 18:04:12 +0100

    fixed build errors, ui fixes

--- pages/auth/login.tsx
+++ pages/auth/login.tsx
@@ -9,8 +9,15 @@ import Link from "next/link";
 const Login: NextPage = () => {
   return (
     <div className="auth-page">
-      <div className="auth-page__placeholder">
-        <Image src={authImg} />
+            <div className="auth-page__placeholder">
+        <div>
+          <Image src={authImg} alt="image placeholder" />
+          <h3 className="auth-page__placeholder-heading">Real Time Updates</h3>
+          <p>
+            Get real time updates on your fluid like temperature, level and
+            device runtime at any time.
+          </p>
+        </div>
       </div>
       <div className="auth-page__form-container">
         <form className="auth-page__form">
@@ -21,7 +28,7 @@ const Login: NextPage = () => {
           </div>
           <Button content="Log in" variant="blue" />
           <p className="auth-page__form-link">
-            Don't have an account? <Link href="/auth/signup">Sign Up</Link>
+            Don&apos;t have an account? <Link href="/auth/signup">Sign Up</Link>
           </p>
         </form>
       </div>
--- pages/auth/signup.tsx
+++ pages/auth/signup.tsx
@@ -1,11 +1,41 @@
-import React from 'react';
+import React from "react";
+import Image from "next/image";
+import type { NextPage } from "next";
+import Button from "../../components/ui/button/Button";
+import Input from "../../components/ui/input/input";
+import authImg from "../../assets/auth-img.svg";
+import Link from "next/link";
 
-const Signup = () => {
-    return (
+const Signup: NextPage = () => {
+  return (
+    <div className="auth-page">
+      <div className="auth-page__placeholder">
         <div>
-            hi
+          <Image src={authImg} alt="image placeholder" />
+          <h3 className="auth-page__placeholder-heading">Real Time Updates</h3>
+          <p>
+            Get real time updates on your fluid like temperature, level and
+            device runtime at any time.
+          </p>
         </div>
-    );
+      </div>
+      <div className="auth-page__form-container">
+        <form className="auth-page__form">
+          <div className="auth-page__form-inputs">
+            <h3 className="auth-page__form-header">Create Account</h3>
+            <Input type="email" placeholder="Email Address" />
+            <Input type="password" placeholder="Password" />
+            <Input type="email" placeholder="Email Address" />
+            <Input type="password" placeholder="Password" />
+          </div>
+          <Button content="Log in" variant="blue" />
+          <p className="auth-page__form-link">
+            Already have an account? <Link href="/auth/login">Log in</Link>
+          </p>
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default Signup;
